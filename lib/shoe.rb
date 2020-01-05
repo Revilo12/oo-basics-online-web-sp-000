e05026d365c83eee46087156c2d3996e9cb62565
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-05 15:57:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,9 @@
 # Make your shoe class here!
 class Shoe 
   attr_accessor :size, :condition, :material, :color 
+  attr_reader :brand
   def initialize(brand)
+    @brand = brand
+  end
+  def cobble
+    @condition = "new"
